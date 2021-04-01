88d221755e15ffe496e8331f527ca120f9c75825
joshcard <66044735+joshcard@users.noreply.github.com>
2021-03-31 18:23:20 -0600

    Add project files.

---
+++ Components/TeamNameViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharpenTheSaw.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpenTheSaw.Components
+{
+    public class TeamNameViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext _context;
+        public TeamNameViewComponent (BowlingLeagueContext ctx)
+            {
+            _context = ctx;
+            }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Teams
+                .Distinct()
+                .OrderBy(x=>x));
+        }
+    }
+}
